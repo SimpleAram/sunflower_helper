e290095628d20126218406f790a4f3feaf11f0ca
SimpleAram <79262606+SimpleAram@users.noreply.github.com>
2023-02-09 14:08:06 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,9 @@
 from cmath import log
-from distutils.sysconfig import PREFIX
 import discord
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
-PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['MTA3MzEwMTU4NzUwMjg3MDYxOQ.GJGjAL.plSNWFd4NookNseLcOigTpK9AqKrzZqrxHc7SA']
 
 client = discord.Client()
@@ -19,10 +17,10 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content == f'{PREFIX}call':
+    if message.content == f'call':
         await message.channel.send("callback!")
 
-    if message.content.startswith(f'{PREFIX}hello'):
+    if message.content.startswith(f'hello'):
         await message.channel.send('Hello!')
 
 
